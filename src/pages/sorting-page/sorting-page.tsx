ffb4b50c5170a91c34bf8fe652f747453c36fae5
Đặng Văn Hậu <121596451+dev-snake@users.noreply.github.com>
2025-04-15 23:28:01 +0700

    fix(ui) : fix animation

--- src/pages/sorting-page/sorting-page.tsx
+++ src/pages/sorting-page/sorting-page.tsx
@@ -1,13 +1,27 @@
+import { cn } from '@/lib/utils';
 import { ChevronRight } from 'lucide-react';
+import { useState } from 'react';
 interface SortingPageProps {
     foo: string;
 }
+interface IActiveMounted {
+    isActive: boolean;
+    count: number;
+}
 
 const SortingPage = (props: SortingPageProps) => {
+    const [isActive, setIsActive] = useState<boolean>(false);
+    const [mounted, setMounted] = useState<IActiveMounted>({
+        isActive: true,
+    } as IActiveMounted);
+    const handleMounted = () => {
+        if (mounted.count === 1) return;
+        setMounted({ isActive: false, count: 1 });
+    };
     return (
         <div>
             <div className="flex gap-x-2 justify-center">
-                {Array.from({ length: 20 }).map((_, idx) => (
+                {Array.from({ length: 22 }).map((_, idx) => (
                     <div
                         key={idx}
                         className="w-full  bg-blue-200 rounded-xs my-2 max-w-14 h-48 text-center"
@@ -16,46 +30,79 @@ const SortingPage = (props: SortingPageProps) => {
                     </div>
                 ))}
             </div>
-            <button className="absolute bottom-8 left-2 py-5 px-2 rounded-xs shadow bg-green-400 hover:cursor-pointer select-none">
-                <ChevronRight className=" text-white " />
-                <div className="absolute left-10 ml-1 bg-green-400 h-full top-0 shadow">
-                    <button className="text-xs w-full h-1/2 px-4 hover:cursor-pointer text-white uppercase hover:bg-green-500">
-                        Create(A)
-                    </button>
-                    <button className="text-xs w-full h-1/2 px-4 hover:cursor-pointer text-white uppercase hover:bg-green-500">
-                        Sort(B)
-                    </button>
+            <div className="absolute bottom-8 left-2  rounded-xs shadow bg-green-400 hover:cursor-pointer select-none">
+                <div className="py-5 px-2 text-white " onClick={() => setIsActive(!isActive)}>
+                    <ChevronRight
+                        className={cn({
+                            'rotate-180 transition-all duration-500': isActive,
+                            'rotate-0 transition-all duration-500': !isActive,
+                        })}
+                        onClick={handleMounted}
+                    />
                 </div>
-                <div className="absolute top-0 left-36 ml-1  h-1/2 py-1">
-                    <div className="flex h-full items-center gap-x-1">
-                        <span className="whitespace-nowrap">N =</span>
-                        <input
-                            type="number"
-                            placeholder="N"
-                            className="bg-white h-full inline-block outline-none text-xs px-2 max-w-14"
-                            defaultValue={10}
-                            max={20}
-                            min={1}
-                        />
-                        <button className="bg-green-400 block text-xs h-full px-2 uppercase text-white tracking-wide rounded-xs hover:cursor-pointer">
-                            random
+                <div
+                    className={cn('', {
+                        block: isActive,
+                        hidden: mounted.isActive,
+                    })}
+                >
+                    <div
+                        className={cn(
+                            'absolute left-10 ml-1  origin-left h-full top-0 transition-all duration-500 ',
+                            {
+                                'before:absolute before:block before:bg-green-400 shadow    before:animation-show-in before:h-full ':
+                                    isActive,
+                                'before:absolute before:block before:bg-green-400 z-20    before:animation-show-out before:h-full ':
+                                    !isActive,
+                            }
+                        )}
+                    >
+                        <button
+                            className={cn('text-xs w-full h-1/2 relative z-10 px-4   uppercase ', {
+                                'text-white hover:bg-green-500 z-0 hover:cursor-pointer': isActive,
+                            })}
+                        >
+                            <span className={cn({ 'opacity-0 transition-all duration-200 text-transparent': !isActive })}>Create(A)</span>
                         </button>
-                        <button className="bg-green-400 block text-xs h-full px-2 uppercase text-white tracking-wide rounded-xs hover:cursor-pointer">
-                            sorted
-                        </button>
-                        <span className="whitespace-nowrap">N =</span>
-                        <input
-                            type="text"
-                            placeholder="1,2,3,4,..."
-                            className="bg-white h-full inline-block outline-none text-xs px-2 min-w-14"
-                            defaultValue={[29, 10, 14, 37, 14].join(',')}
-                        />
-                        <button className="bg-green-400 block text-xs h-full px-2 uppercase text-white tracking-wide rounded-xs hover:cursor-pointer">
-                            Go
+                        <button
+                            className={cn('text-xs w-full h-1/2 relative z-10 px-4   uppercase transi ', {
+                                'text-white hover:bg-green-500 z-0 hover:cursor-pointer': isActive,
+                            })}
+                        >
+                            <span className={cn({ 'opacity-0 transition-all duration-200 text-transparent': !isActive })}>Sort(B)</span>
                         </button>
                     </div>
+                    <div className="absolute top-0 left-36 ml-1  h-1/2 py-1 hidden">
+                        <div className="flex h-full items-center gap-x-1">
+                            <span className="whitespace-nowrap">N =</span>
+                            <input
+                                type="number"
+                                placeholder="N"
+                                className="bg-white h-full inline-block outline-none text-xs px-2 max-w-14"
+                                defaultValue={10}
+                                max={22}
+                                min={1}
+                            />
+                            <button className="bg-green-400 block text-xs h-full px-2 uppercase text-white tracking-wide rounded-xs hover:cursor-pointer">
+                                random
+                            </button>
+                            <button className="bg-green-400 block text-xs h-full px-2 uppercase text-white tracking-wide rounded-xs hover:cursor-pointer">
+                                sorted
+                            </button>
+                            <span className="whitespace-nowrap">N =</span>
+                            <input
+                                type="text"
+                                placeholder="1,2,3,4,..."
+                                className="bg-white h-full inline-block outline-none text-xs px-2 min-w-14"
+                                defaultValue={[29, 10, 14, 37, 14].join(',')}
+                            />
+                            <button className="bg-green-400 block text-xs h-full px-2 uppercase text-white tracking-wide rounded-xs hover:cursor-pointer">
+                                Go
+                            </button>
+                        </div>
+                    </div>
                 </div>
-            </button>
+            </div>
         </div>
     );
 };
